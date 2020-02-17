da47aa4a34a9dd1969b1a9579a278c460fbfd32f
NealKemissi <neal.k@hotmail.fr>
2020-02-17 10:58:00 +0100

    typescript

---
+++ index.js
@@ -0,0 +1,3 @@
+import { Pokemon } from "pokemon.js";
+let pokemon = new Pokemon("Pikachu");
+console.log('nom du pokemon : ' + pokemon.name);
---
+++ pokemon.ts
@@ -0,0 +1,10 @@
+export class Pokemon {
+    
+    /** nom du pokemon **/
+    private name : string;
+
+    /** constructeur **/
+    constructor(name : string) {
+        this.name = name;
+    }
+}
